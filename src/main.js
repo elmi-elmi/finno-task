9388466d9d9fd55a18c4fb07bdd1b0748dc35c5b
elmi-elmi <shahrokhelmi@gmail.com>
2022-09-14 13:03:11 +0430

    add progressbar to all routes

--- src/main.js
+++ src/main.js
@@ -5,14 +5,17 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import Vuelidate from 'vuelidate'
 import axios from 'axios'
+import i18n from './includes/i18n';
 import 'nprogress/nprogress.css'
+import ProgressBar from './includes/progress-bar.js';
 Vue.use(Vuelidate)
 Vue.config.productionTip = false
-
+ProgressBar(router);
 new Vue({
   router,
   store,
   vuetify,
+  i18n,
   created(){
     // auto login
     const userString = localStorage.getItem('user')
